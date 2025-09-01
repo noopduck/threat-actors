2426ca03ae67740b7ce32e5a646a47a925544ae8
noopduck <noop@dev.null>
2025-09-01 22:59:45 +0200

    API part works  curl localhost:8080/mitreThreatGroup --silent |jq '.[]| {Name, Description} | select(.Description| contains(organizations) )'

--- cmd/threatactors/main.go
+++ cmd/threatactors/main.go
@@ -2,18 +2,40 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"threatactors/internal/parser"
 	"threatactors/internal/webclient"
 )
 
-func main() {
-	// This is a placeholder main function.
-	// The actual implementation would go here.
-	rawDocument := webclient.GetGroups()
+func mitreThreatGroupsHandler(w http.ResponseWriter, request *http.Request) {
 
-	// fmt.Println(rawDocument)
+	rawDocument := webclient.GetGroups()
 
 	parsedDocument := parser.ParseHTMLTable(rawDocument)
 
-	fmt.Println(parsedDocument)
+	w.Write([]byte(parsedDocument))
+}
+
+func startAPI() {
+
+	http.HandleFunc("/mitreThreatGroup", mitreThreatGroupsHandler)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Fatal when trying to start http listener", err.Error())
+	}
+
+}
+
+func main() {
+	// This is a placeholder main function.
+	// The actual implementation would go here.
+	//rawDocument := webclient.GetGroups()
+
+	// fmt.Println(rawDocument)
+
+	//parsedDocument := parser.ParseHTMLTable(rawDocument)
+
+	//fmt.Println(parsedDocument)
+	//
+	startAPI()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module threatactors
 
 go 1.25.0
+
+require golang.org/x/net v0.43.0 // indirect

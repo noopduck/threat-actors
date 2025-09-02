46e48ebd00d9aeaea050d3c832ef016297abb64a
Noopduck <rahmiac@protonmail.com>
2025-09-02 09:44:14 +0200

    Added search handler function for single word occurrence search

--- cmd/threatactors/main.go
+++ cmd/threatactors/main.go
@@ -4,37 +4,71 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"threatactors/internal/parser"
 	"threatactors/internal/webclient"
 )
 
 func mitreThreatGroupsHandler(w http.ResponseWriter, request *http.Request) {
+	rawDocument := webclient.GetGroups()
+
+	parsedDocument := parser.ParseHTMLTable(rawDocument)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(parsedDocument))
+}
+
+func mitreThreatGroupSearchHandler(w http.ResponseWriter, request *http.Request) {
+	searchTerm := strings.ToLower(request.URL.Query().Get("searchTerm"))
 
 	rawDocument := webclient.GetGroups()
 
 	parsedDocument := parser.ParseHTMLTable(rawDocument)
 
-	w.Write([]byte(parsedDocument))
+	var parsedDocumentJSON []parser.Row
+	err := json.Unmarshal(parsedDocument, &parsedDocumentJSON)
+	if err != nil {
+		http.Error(w, "Error processing request", http.StatusInternalServerError)
+		return
+	}
+
+	var results []parser.Row
+	for _, row := range parsedDocumentJSON {
+		if strings.Contains(strings.ToLower(row.ID), searchTerm) ||
+			strings.Contains(strings.ToLower(row.Name), searchTerm) ||
+			strings.Contains(strings.ToLower(row.Description), searchTerm) {
+
+			results = append(results, row)
+		}
+	}
+
+	response, err := json.Marshal(results)
+	if err != nil {
+		http.Error(w, "Error processing request", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
 }
 
 func startAPI() {
-
 	http.HandleFunc("/mitreThreatGroup", mitreThreatGroupsHandler)
+	http.HandleFunc("/mitreThreatGroupSearch", mitreThreatGroupSearchHandler)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Fatal when trying to start http listener", err.Error())
 	}
-
 }
 
 func main() {
 	// This is a placeholder main function.
 	// The actual implementation would go here.
-	//rawDocument := webclient.GetGroups()
+	// rawDocument := webclient.GetGroups()
 
 	// fmt.Println(rawDocument)
 
-	//parsedDocument := parser.ParseHTMLTable(rawDocument)
+	// parsedDocument := parser.ParseHTMLTable(rawDocument)
 
 	//fmt.Println(parsedDocument)
 	//

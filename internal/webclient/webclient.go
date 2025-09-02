051f7e733ba1200790010f09a9160320f55d114f
Noopduck <rahmiac@protonmail.com>
2025-09-02 13:46:11 +0200

    Update webclient.go

--- internal/webclient/webclient.go
+++ internal/webclient/webclient.go
@@ -2,25 +2,40 @@
 package webclient
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 )
 
-func GetGroups() string {
-	response, err := http.Get("https://attack.mitre.org/groups/")
+func GetPage(path string, group *string) string {
+	if group != nil {
+		path += *group
+	}
+
+	fmt.Println("Fetching URL:", path)
+
+	response, err := http.Get(path)
 	if err != nil {
 		println("Error fetching groups:", err)
 	}
 	defer response.Body.Close()
 
-	// Process the response body as needed
-	// For now, just return an empty string as a placeholder
-
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		println("Error reading response body:", err)
-		return ""
 	}
 
 	return string(body)
+}
+
+// GetGroups Get all groups
+func GetGroups() string {
+	body := GetPage("https://attack.mitre.org/groups/", nil)
+	return body
+}
+
+// GetGroup Get detailed group information
+func GetGroup(group string) string {
+	body := GetPage("https://attack.mitre.org/groups/", &group)
+	return body
 }
